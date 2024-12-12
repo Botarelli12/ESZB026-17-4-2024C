6cf4d82246f3809e3bae66ac4bdb9aa820592fb1
Giovanna <bla.borarelli@po>
2024-12-11 23:27:43 -0200

    codigo arduino

--- projeto/thermistor/thermistor.ino
+++ projeto/thermistor/thermistor.ino
@@ -1,14 +1,29 @@
-#include <Thermistor.h> //INCLUSÃO DA BIBLIOTECA
- 
+#include "Thermistor.h" //INCLUSÃO DA BIBLIOTECA
+
+#define LED 9
+#define BUZ 8
+#define limite 30
+
 Thermistor temp(2); //VARIÁVEL DO TIPO THERMISTOR, INDICANDO O PINO ANALÓGICO (A2) EM QUE O SENSOR ESTÁ CONECTADO
 void setup() {
   Serial.begin(9600); //INICIALIZA A SERIAL
   delay(1000); //INTERVALO DE 1 SEGUNDO
+  pinMode(LED, OUTPUT);
+  pinMode(BUZ, OUTPUT);
 }
 void loop() {
-  int temperature = temp.getTemp(); //VARIÁVEL DO TIPO INTEIRO QUE RECEBE O VALOR DE TEMPERATURA CALCULADO PELA BIBLIOTECA
+  float temperature = temp.getTemp(); //VARIÁVEL DO TIPO INTEIRO QUE RECEBE O VALOR DE TEMPERATURA CALCULADO PELA BIBLIOTECA
   Serial.print("Temperatura: "); //IMPRIME O TEXTO NO MONITOR SERIAL
   Serial.print(temperature); //IMPRIME NO MONITOR SERIAL A TEMPERATURA MEDIDA
   Serial.println("*C"); //IMPRIME O TEXTO NO MONITOR SERIAL
-  delay(1000); //INTERVALO DE 1 SEGUNDO
+  
+  if (temperature >= limite){
+    digitalWrite(LED, HIGH);
+    digitalWrite(BUZ, HIGH);
+  }
+  else{
+    digitalWrite(LED, LOW);
+    digitalWrite(BUZ, LOW);
+  }
+  delay(100); //INTERVALO DE 1 SEGUNDO
 }
